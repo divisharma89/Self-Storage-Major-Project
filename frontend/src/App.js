f3c078855e104faf87af776d8ab407fbcde270c7
SUDHANSHU SINGH <Sudhanshusingh705495@gmail.com>
2023-01-24 11:04:32 +0530

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Main from './components/main';
 import Login from './components/main/Login';
 import Signup from './components/main/Signup';
+import Footer from './components/main/Footer';
 import Admin from './components/admin';
 import User from './components/user';
 import AdminProfile from './components/admin/Profile';
@@ -25,6 +26,7 @@ function App() {
           <Route path="home" element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
+            <Route path="footer" element={<Footer />} />
           </Route>
           
           <Route element={<Admin />} path="admin">
--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import  '../main/Home.css'
 
-const Home = () => {
+const Home = (props) => {
   return (
-    <div>Home</div>
-  )
-}
-
-export default Home
+      <div>
+          <div className="banner-container">
+              <div className="text-center">
+                  <h1 className=" text-6xl text-orange-600 font-bold">Welcome to Self-Storage</h1>
+                  <h4 className="text-4xl mt-8 text-white">Your very own personal self-storage corner</h4>
+              </div>
+          </div>
+      </div>
+  );
+};
+export default Home;
